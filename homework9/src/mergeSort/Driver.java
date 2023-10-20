3ea5c47dad25445cf4062b83a7362dcd870c5cfd
paigestickney <75600165+paigestickney@users.noreply.github.com>
2023-10-20 10:19:08 -0500

    hw 9

---
+++ homework9/src/mergeSort/Driver.java
@@ -0,0 +1,71 @@
+package mergeSort;
+
+public class Driver {
+	package pack;
+
+	public class MergeSort {
+
+		public class MergeSort {
+			private static void merge (int[] numbers, int i, int j, int k) {
+				int mergedSize = k - i +1;
+				int[] mergedNumbers = new int[mergedSize];
+				
+				int mergePosition = 0;
+				int leftPosition = i;
+				int rightPosition = j + 1;
+				
+				while (leftPosition <= j && rightPosition <= k) {
+					if (numbers[leftPosition] <= numbers[rightPosition]) {
+						mergedNumbers[mergePosition] = numbers[leftPosition];
+						leftPosition ++;
+					}
+					else {
+						mergedNumbers[mergePosition] = numbers[rightPosition];
+						rightPosition ++;
+					}
+					mergePosition ++;
+				}
+				
+				while (leftPosition <= j) {
+					mergedNumbers[mergePosition] = numbers[leftPosition];
+					leftPosition ++;
+					mergePosition ++;
+				}
+				
+				while (rightPosition <= k) {
+					mergedNumbers[mergePosition] = numbers[rightPosition];
+					rightPosition ++;
+					mergePosition ++;
+				}
+				
+				for (mergePosition = 0; mergePosition < mergedSize; mergePosition ++) {
+					numbers[i + mergePosition] = mergedNumbers[mergePosition];
+				}
+			}
+			
+			
+			// merge sort function
+			private static void mergeSort (int [] numbers, int i, int k) {
+				int j = 0;
+				
+				if (i < k) {
+					j = (i + k) / 2;
+					
+					mergeSort(numbers, i, j);
+					mergeSort(numbers, j + 1, k);
+					merge(numbers, i, j, k);
+				}
+			}
+			
+			// main function
+			public static void main (String[] args) {
+				int [] numbers = {3, 26, 15, 18, 31, 7, 6, 22};
+				
+				mergeSort(numbers, 0, numbers.length - 1);
+				System.out.println("The sorted list is: " + Arrays.toString(numbers));
+			}
+		}
+	}
+
+
+}
