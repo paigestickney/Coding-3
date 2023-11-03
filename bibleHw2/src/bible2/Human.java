8278787f53672c1ba74f5822b82c655e10cf74a4
paigestickney <75600165+paigestickney@users.noreply.github.com>
2023-11-03 00:23:52 -0500

    biblehw2

---
+++ bibleHw2/src/bible2/Human.java
@@ -0,0 +1,11 @@
+package bible2;
+
+public class Human {
+	public static int MaxAge = 72;
+	public static int AgeAsApprentice = 18;
+	public static int YearToBeDisciple = 3;
+	public static int AgeOfGivingBirth = 30;
+	public static int CapOfApprentice = 2;
+	public static int CapOfBaby = 1;
+
+}
