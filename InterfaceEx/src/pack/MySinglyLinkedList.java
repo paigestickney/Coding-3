05bdb023a54a4bf6462b608b3e8fd74bdc44cec4
paigestickney <75600165+paigestickney@users.noreply.github.com>
2023-11-06 23:17:40 -0600

    work in class

--- InterfaceEx/src/pack/MySinglyLinkedList.java
+++ InterfaceEx/src/pack/MySinglyLinkedList.java
@@ -11,11 +11,60 @@ public class MySinglyLinkedList {
 	
 	// implement insert after
 	public boolean InsertAfter(int n, int o) {
-		if (size == 0) {
+		MyNode currNode = this.SearchNode(n);
+		if (currNode == null) {
 			return false;
 		}
-		// search for n, then insert after n
-		
+		else {
+			MyNode newNode = new MyNode();
+			newNode.Data = o;
+			MyNode nextNode = currNode.NextNode;
+			
+			currNode.NextNode = newNode;
+			newNode.NextNode = nextNode;
+			this.Size++;
+			return true;
+		}
+	}
+	
+	// remove after function
+	public boolean RemoveAfter(int n) {
+		MyNode currNode = this.SearchNode(n);
+		if (currNode == null) {
+			return false;
+		}
+		else {
+			MyNode nextNode = currNode.NextNode;
+			if (nextNode == null) {
+				return true;
+			}
+			MyNode nextNextNode = nextNode.NextNode;
+			if (nextNextNode == null) {
+				currNode.NextNode = null;
+				return true;
+
+			}
+			else {
+				currNode.NextNode = nextNextNode;
+				nextNode.NextNode = null;
+				this.Size--;
+				return true;
+			}
+		}
+	}
+	
+	// return the first instance of the node whose payload matches the value that we are looking for
+	// return null if we don't find the value
+	public boolean Search (int n) {
+		MyNode currNode = this.Header;
+		while (currNode != null) {
+			if (currNode.Data == n) {
+				return currNode;
+			}
+			else {
+				currNode = currNode.NextNode;
+			}
+		}
 	}
 
 }
---
+++ InterfaceEx/src/pack/MySinglyLinkedListList.java
@@ -0,0 +1,65 @@
+package pack;
+
+public class MySinglyLinkedListList implements MyList {
+
+	@Override
+	public void Appdnd(int x) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void Prepend(int x) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean InsertAfter(int w, int x) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean Remove(int x) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int SearchNode(int x) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void Print() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void PrintReverse() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void Sort() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean IsEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int GetLength() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
